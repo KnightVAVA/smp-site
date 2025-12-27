e45568036ef5ae3fb19924ba7f2acade8fa29ac3
KnightVAVA <jedricknight@gmail.com>
2025-12-27 17:39:24 +0800

    Update: remove /dist in .gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/SMP-Site/",
+  base: "/smp-site/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
